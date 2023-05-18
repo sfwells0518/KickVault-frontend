3e2a36da2414214a331d3cb5cbb98cedd4d865a4
Sam Wells <sfwells0518@gmail.com>
2023-05-18 13:51:54 -0500

    install and implement bootstrap css for styling

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,17 +1,22 @@
 export function ProductsIndex(props) {
+  console.log(props.products);
   return (
-    <div>
-      <h3>All Products</h3>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <h5>{product.description}</h5>
-          <p>{product.price}</p>
-          <img src={product.image.url} />
-        </div>
-
-      ))}
+    <div id="products-index" className="mt-4">
+      <h1>All Products</h1>
+      <div className="row">
+        {props.products.map((product) => (
+          <div key={product.id} className="col-sm-4 mb-3">
+            <div className="card h-100">
+              <div className="card-body card-body-custom d-flex flex-column">
+                <h3 className="card-title">{product.name}</h3>
+                <img src={product.images[0].url} alt="Product Image" className="mb-3" />
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-
+  );
 }
--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -2,8 +2,9 @@ export function ProductsShow(props) {
   return (
     <div>
       <p>Name: {props.product.name}</p>
-      <p>Description: {props.product.name}</p>
-      <p>Image(s): {props.product.image.url}</p>
+      <p>Description: {props.product.description}</p>
+      <p>Price: {props.product.price}</p>
+      <p>Image(s): {props.product.images.url}</p>
     </div>
   );
 }
