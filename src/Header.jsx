fbc349a6e2b6268f9c14631440572a358349c941
Sam Wells <sfwells0518@gmail.com>
2023-05-23 11:10:14 -0500

    remove search form and button from nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -16,89 +16,87 @@ export function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="nav nav-underline">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Wishlist
-              </a>
-            </li>
 
-            <li className="nav-item dropdown">
-              <a
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="ms-auto d-flex">
+            <ul className="nav nav-underline">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                Wishlist
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
                 className="nav-link dropdown-toggle text-primary"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
-              >
-                Shoe Categories
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
+                >
+                Categories
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Lifestyle
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Airmax
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Jordan
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2 center-placeholder" type="search" placeholder="Product Search" aria-label="Search" />
-            <button className="black-search-button" type="submit">
-              Search
-            </button>
-          </form>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link text-primary" href="#">
-                <img
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link text-primary" href="#">
+                  <img
                   src="https://www.freeiconspng.com/thumbs/bags-icon/bag-icon-6.png"
                   alt="Bag"
                   style={{ width: "20px", marginRight: "5px" }}
-                />
-                Bag
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
+                  />
+                  Bag
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
                 className="nav-link dropdown-toggle text-primary"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
-              >
+                >
                 Profile
-              </a>
-              <ul className="dropdown-menu dropdown-menu-end">
-                <li>
-                  <a className="dropdown-item" href="#">
+                </a>
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Register
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Sign In
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
                     Log Out
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
